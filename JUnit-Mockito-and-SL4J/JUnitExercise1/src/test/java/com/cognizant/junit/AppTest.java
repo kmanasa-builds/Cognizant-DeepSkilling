9b1cf5f350e5f8e84a3f9271be3c90ad91a1fd5f
Kondapalli Manasa <kondapallimanasa22@gmail.com>
2026-07-08 20:44:16 +0530

    Completed JUnit Exercise 1 - Setting Up JUnit

---
+++ JUnit-Mockito-and-SL4J/JUnitExercise1/src/test/java/com/cognizant/junit/AppTest.java
@@ -0,0 +1,13 @@
+package com.cognizant.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
