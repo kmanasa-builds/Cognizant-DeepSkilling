95d4c3555efaf40a2a20e9ddc61bc6f3bfc434c7
Kondapalli Manasa <kondapallimanasa22@gmail.com>
2026-07-10 20:59:46 +0530

    Completed JUnit Exercise 3 - Assertions in JUnit

---
+++ JUnit-Mockito-and-SL4J/JUnitExercise5/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.cognizant.junit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(15, calculator.add(10, 5));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(5, calculator.subtract(10, 5));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(50, calculator.multiply(10, 5));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2, calculator.divide(10, 5));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0));
+    }
+}
