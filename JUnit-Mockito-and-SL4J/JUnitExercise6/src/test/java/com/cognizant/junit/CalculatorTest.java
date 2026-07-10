63e081a1142d44d55f92fe93dff57b8e2db5f69e
Kondapalli Manasa <kondapallimanasa22@gmail.com>
2026-07-10 21:15:31 +0530

    Completed JUnit Exercise 4 - Arrange-Act-Assert Pattern, Test Fixtures, Setup and Teardown

---
+++ JUnit-Mockito-and-SL4J/JUnitExercise6/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.cognizant.junit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Setting up test...");
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Cleaning up after test...");
+        calculator = null;
+    }
+
+    @Test
+    void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
